1f80ab14cc3b66ee676ee5c7ce93e586e8d05bbe
Gabriele Salvo <72619479+gabsalvo@users.noreply.github.com>
2021-03-18 13:24:32 +0000

    Add files via upload

---
+++ challenges.js
@@ -0,0 +1,38 @@
+'use strict';
+const answerPoll = document.querySelector('.poll');
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write Option Number)`
+      )
+    );
+    if (answer >= 0 && answer <= 3) {
+      this.answers[answer]++;
+    } else {
+      const wrong = alert('Wrong answer! Try again by pressing OK');
+      this.registerNewAnswer();
+    }
+    console.log(poll.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+answerPoll.addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
